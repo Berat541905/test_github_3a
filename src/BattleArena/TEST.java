1e844932e29638f45a8cd35758e8711e48c1e2f5
Berat541905 <Berat.Tuer@hak-reutte.ac.at>
2024-03-05 12:44:22 +0100

    Prject

--- src/BattleArena/TEST.java
+++ src/BattleArena/TEST.java
@@ -6,7 +6,7 @@ package BattleArena;
 				
 				battleArena b1 =new battleArena();
 				Character p1 = new Dragon("GRIMALDO GRIMUERES");
-				Character p2 = new Dwarf("FERID ABDULKERIMALIDOGLU");
+				Character p2 = new Dwarf("FERDI ABDULKERIMALIDOGLU");
 				b1.fight(p1, p2);
-			}
-}
+		}
+	}
