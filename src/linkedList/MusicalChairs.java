61eead16e83fd83caa6f6be5597c123b3fdab9c7
Berat541905 <Berat.Tuer@hak-reutte.ac.at>
2024-03-12 13:02:50 +0100

    .

--- src/linkedList/MusicalChairs.java
+++ src/linkedList/MusicalChairs.java
@@ -1,9 +1,11 @@
 package linkedList;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MusicalChairs {
 	
@@ -22,5 +24,25 @@ public class MusicalChairs {
 		}
 		return tmp;
 	}
+	public void rotate(int distance) {
+		Collections.rotate(this.names, distance);
+	}
 	
+	public void rotateAndRemoveLast(int distance) {
+		Collections.rotate(this.names, distance);
+		((LinkedList<String>) this.names).removeLast();
+	
+	}
+	public String play(int distance) {
+        if(!this.names.isEmpty()) {
+        	while(this.names.size() > 1) {
+        	int randomNumber = ThreadLocalRandom.current().nextInt();
+        	this.rotateAndRemoveLast(randomNumber);
+        	System.out.println(this.names);
+        	}
+        }
+        //optional
+        return this.names.get(0);
+	}
+
 }
