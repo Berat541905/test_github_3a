7410673a94c700edde8b9d8c8a0c43fb35268931
Berat541905 <Berat.Tuer@hak-reutte.ac.at>
2024-03-19 22:12:48 +0100

    Finish

--- src/BattleArena/APP.java
+++ src/BattleArena/APP.java
@@ -5,8 +5,8 @@ package BattleArena;
 		public static void main(String[] args) {
 				
 				battleArena b1 =new battleArena();
-				Character p1 = new Dragon("MARADONAAAAAAAA");
-				Character p2 = new Dwarf("PELEEEEEEEE");
+				Character p1 = new Dragon("Berat");
+				Character p2 = new Dwarf("Arda");
 				b1.fight(p1, p2);
 		}
 	}
